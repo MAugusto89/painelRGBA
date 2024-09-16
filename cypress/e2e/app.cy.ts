cb7fb8df3b01f0e78cc6c1d22a2a064ce228ffcb
MAgusto89 <souzadagama966.masdg@gmail.com>
2024-09-16 00:48:56 -0400

    Aplicando CI/CD

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -11,11 +11,8 @@ describe("App", () => {
     cy.get("[data-cy=red-slider]")
       .then((input: JQuery<HTMLInputElement>) => input[0].stepUp(red))
       .trigger("change");
-    cy.wait(250);
     cy.get("[data-cy=green-slider]").type(green.toString()).trigger("change");
-    cy.wait(250);
     cy.get("[data-cy=blue-slider]").type(blue.toString()).trigger("change");
-    cy.wait(250);
     cy.get("[data-cy=alpha-slider]")
       .then((input: JQuery<HTMLInputElement>) => input[0].stepDown(alpha))
       .trigger("change");
